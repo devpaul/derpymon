4d2db8b526c30f58bb7d2859c221be304d733f00
Paul Shannon <a@devpaul.com>
2017-08-20 11:17:09 -0700

    dev server cleanup & reporting of local ip address

--- support/webserv/index.ts
+++ support/webserv/index.ts
@@ -1,17 +1,17 @@
 import createServer, { ServerType } from 'webserv/commands/createServer';
-import opn = require('opn');
 import WebProxy from 'webserv/middleware/WebProxy';
 import route from 'webserv/handlers/route';
 import ServeFile from 'webserv/middleware/ServeFile';
 import { setLogLevel } from 'webserv/log';
 import LogRequest from 'webserv/middleware/LogRequest';
-import { ServerResponse } from 'http';
+import startWelcomeServer from './welcomeServer';
 
 const proxy = new WebProxy('http://localhost:9999', {
 	ws: true
 });
 
 setLogLevel('debug');
+startWelcomeServer();
 
 createServer({
 	middleware: [
@@ -42,28 +42,4 @@ createServer({
 	upgrade: proxy
 }).then((server) => {
 	server.start()
-});
-
-createServer({
-	middleware: [(_request: any, response: ServerResponse) => {
-		response.write(`
-			<html>
-			<body>
-				<ul>
-					<li><a href="http://localhost:8010">Local dev server</a> (proxies to webpack dev server)</li>
-					<li><a href="https://localhost:8001">HTTPS server</a> (uses build in dist)</li>
-				</ul>
-			</body>
-			</html>
-		`);
-		response.end();
-	}],
-	port: 8888
-}).then((server) => {
-	return server.start().then(() => {
-		console.log(`server started on ${ server.port }`);
-		opn(`http://localhost:${ server.port }`);
-	});
-}, () => {
-	console.error('server failed to start');
 });
