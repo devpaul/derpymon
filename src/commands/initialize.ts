967b988398e94bdd2c4bb11cb4b644d2329b8c3f
Paul Shannon <a@devpaul.com>
2017-12-19 10:29:26 -0700

    WIP commands

--- src/commands/initialize.ts
+++ src/commands/initialize.ts
@@ -1,8 +1,12 @@
-import Executor from '../framework/Executor';
 import AppContext, { ApplicationState } from '../context/AppContext';
 import registerHeightComponent from '../components/heightComponent';
+import AssetContext from '../context/AssetContext';
+import OutsideContext from '../context/OutsideContext';
+import monsters from '../configuration/monsters';
+import registerMonsters from './registerMonsters';
+import randomizeEncounter from './randomizeEncounter';
 
-export default function initialize(app: AppContext, executor: Executor) {
+export default function initialize(app: AppContext, assets: AssetContext, outside: OutsideContext) {
 	if (app.state !== ApplicationState.Initial) {
 		throw new Error('Application already initialized');
 	}
@@ -10,15 +14,19 @@ export default function initialize(app: AppContext, executor: Executor) {
 	app.isLoadingState = true;
 
 	if (!app.initialized.monsters) {
-		executor.execute(ActionType.LoadMonsters);
+		// TODO NOTE: This is the problem w/ multi-state w/o an executor to inject
+		// commands accumulate requirements from their children making it a mess to unwind
+		registerMonsters(monsters, assets, outside);
+		app.initialized.monsters = true;
 	}
+
 	if (!app.initialized.aframe) {
 		registerHeightComponent();
 		app.initialized.aframe = true;
 	}
 
 	if (app.initialized.monsters && app.initialized.aframe) {
-		executor.execute(ActionType.RandomizeEncounter);
+		randomizeEncounter(outside);
 		app.isLoadingState = false;
 		app.state = ApplicationState.Outside;
 	}
--- src/commands/loadMonsters.ts
+++
@@ -1,9 +0,0 @@
-import Executor, { Action } from '../framework/Executor';
-import monsters from '../configuration/monsters';
-import loadedMonsters from './loadedMonsters';
-
-export default function loadMonsters(executor: Executor) {
-	// TODO eventually monsters can come from an async load
-	loadedMonsters()
-	executor.execute('loadedMonsters', monsters);
-}
